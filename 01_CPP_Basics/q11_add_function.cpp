02e990605720c830044b9cd1a9e66a0fdc708e0d
Thirtha Chandra K C <thirthachandra2023@gmail.com>
2026-07-27 22:26:59 +0530

    Add function to perform addition of two numbers

---
+++ 01_CPP_Basics/q11_add_function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void add(int num1,int num2,int num3){
+    num3=num1+num2;
+    cout << "The sum of two numbers is " << num3;
+}
+int main(){
+    int num1,num2,num3;
+    cout << "Enter two numbers to add: ";
+    cin >>num1 >>num2;
+    add(num1,num2,num3);
+    return 0;
+
+}
