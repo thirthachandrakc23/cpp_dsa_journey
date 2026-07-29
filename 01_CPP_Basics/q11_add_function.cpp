6bf80f3c3081c614a03ad13217b40542e899c4a0
Thirtha Chandra K C <thirthachandra2023@gmail.com>
2026-07-29 19:50:16 +0530

    Fix addition logic in add function and update output message; add square pattern program

--- 01_CPP_Basics/q11_add_function.cpp
+++ 01_CPP_Basics/q11_add_function.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 void add(int num1,int num2,int num3){
-    num3=num1+num2;
-    cout << "The sum of two numbers is " << num3;
+    num3=num1=num2;
+    cout << "The addition of two number is " << num3;
 }
 int main(){
     int num1,num2,num3;
